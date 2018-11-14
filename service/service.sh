e24ae372d56f70ebf860bc606b3c13f4534bc268
doozz <276801200@qq.com>
2018-11-14 12:04:03 +0800

    service

--- service/service.sh
+++ service/service.sh
@@ -6,18 +6,24 @@ cd `dirname $0`
 
 script=$0
 action=$1
-service=("mysql","redis","nginx","php")
+
 case $action in
   "build")
-    for file in $service
+    for file in "redis" "mysql" "nginx" "php"
     do
       cd $file 
       docker-compose up -d
-      docker-compose down 
-      docker rm "${file}_service"
-      docker rm "$file"
+      docker-compose down
+      if [ $file == "mysql" ]
+      then
+      	docker rmi "${file}:5.6"
+      elif [ $file == "php" ]
+      then
+	docker rmi "${file}:7.2"
+      else
+        docker rmi $file
+      fi
       cd ../
-      echo "{$file}_service images is done"
     done
     echo "ready to start"
   ;;
